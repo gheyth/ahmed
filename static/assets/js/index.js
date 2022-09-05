d8ec702883409161d4d5787e8182e394364acfb1
fares <djefaflia.fares@gmail.com>
2022-09-05 16:12:14 +0100

    add element and fix js

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -2,9 +2,23 @@
 let navOpen = document.querySelector(".icon-resp")
 let list = document.getElementsByTagName("aside")
 let AllContent = document.getElementById("allcontent")
+
 navOpen.addEventListener('click', () => {
+  OpenAside()
+})
+
+///
+let LinksAside = document.querySelectorAll(".all-bab a")
+
+LinksAside.forEach(element => {
+  element.addEventListener("click", () => {
+    OpenAside()
+  })
+});
+
+function OpenAside() {
   navOpen.children[0].classList.add("hidden")
   navOpen.children[1].classList.toggle("hidden")
   navOpen.children[2].classList.toggle("hidden")
   list[0].classList.toggle("aside-open")
-})
+}
